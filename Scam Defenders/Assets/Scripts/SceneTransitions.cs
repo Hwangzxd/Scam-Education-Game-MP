a09871ff21fbad92562b9268cdf8ac2b5f02f34d
sugondesenoots <martinmangahas972005@gmail.com>
2024-07-08 10:47:34 +0800

    Chats #1
    -Finished Chatting UI

---
+++ Scam Defenders/Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        //Gets the current active scene + Calculates next scene index
+        Scene currentScene = SceneManager.GetActiveScene();
+        int nextSceneIndex = currentScene.buildIndex + 1;
+
+        //Checks if the next scene index is within the valid range
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            //Loads the next scene
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No more scenes in the build settings.");
+        }
+    }
+
+    public void LoadLastScene()
+    {
+        //Gets the current active scene + Calculates last scene index
+        Scene currentScene = SceneManager.GetActiveScene();
+        int lastSceneIndex = currentScene.buildIndex - 1;
+
+        //Checks if the next scene index is within the valid range
+        if (lastSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            //Loads the next scene
+            SceneManager.LoadScene(lastSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No more scenes in the build settings.");
+        }
+    }
+}
