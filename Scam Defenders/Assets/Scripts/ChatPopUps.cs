b820314897487b49e54ced365be67b0408c1265f
sugondesenoots <martinmangahas972005@gmail.com>
2024-07-11 14:03:52 +0800

    changes #1

--- Scam Defenders/Assets/Scripts/ChatPopUps.cs
+++ Scam Defenders/Assets/Scripts/ChatPopUps.cs
@@ -7,9 +7,6 @@ public class ChatPopUps : MonoBehaviour
     public GameObject winPop;
     public GameObject losePop;
 
-    public GameObject winPoints; 
-    //public GameObject losePoints;
-
     public void Report()
     {
         Debug.Log("Report clicked");
@@ -36,16 +33,12 @@ public class ChatPopUps : MonoBehaviour
         {
             losePop.SetActive(false);
             winPop.SetActive(false);
-
-            winPoints.SetActive(true);
         } 
 
         if (!winPop.activeSelf && losePop.activeSelf)
         {
             losePop.SetActive(false);
             winPop.SetActive(false); 
-             
-            //losePoints.SetActive(true);
         }
     }
 }
