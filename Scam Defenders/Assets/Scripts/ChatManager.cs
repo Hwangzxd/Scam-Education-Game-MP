252f3c48f2d85a7468264a306a005c87c9cb6a16
Hwang <chrishuangyc@gmail.com>
2024-08-02 15:55:20 +0800

    Changed app transition speed, added back button in age selection screen, added time's up indicator, continued improving chat system

--- Scam Defenders/Assets/Scripts/ChatManager.cs
+++ Scam Defenders/Assets/Scripts/ChatManager.cs
@@ -8,10 +8,12 @@ public class ChatManager : MonoBehaviour
     public ChatScenario currentScenario;
     public Transform chatContent; // The parent object for chat messages
     public Transform responseContent; // The parent object for response buttons
-    public GameObject messagePrefab; // Prefab for chat messages
+    public GameObject scammerMessagePrefab; // Prefab for scammer chat messages
+    public GameObject playerMessagePrefab; // Prefab for player chat messages
     public GameObject responseButtonPrefab; // Prefab for response buttons
 
     private int currentMessageIndex = 0;
+    private List<GameObject> activeResponseButtons = new List<GameObject>();
 
     void Start()
     {
@@ -27,21 +29,20 @@ public class ChatManager : MonoBehaviour
         }
 
         ChatMessage message = currentScenario.messages[messageIndex];
-        GameObject messageGO = Instantiate(messagePrefab, chatContent);
+        GameObject messageGO = Instantiate(scammerMessagePrefab, chatContent);
         TMP_Text messageText = messageGO.GetComponentInChildren<TMP_Text>();
         messageText.text = message.messageText;
 
-        if (!message.isPlayerMessage)
-        {
-            foreach (ChatResponse response in message.responses)
-            {
-                GameObject responseGO = Instantiate(responseButtonPrefab, responseContent);
-                TMP_Text responseText = responseGO.GetComponentInChildren<TMP_Text>();
-                responseText.text = response.responseText;
 
-                Button responseButton = responseGO.GetComponentInChildren<Button>();
-                responseButton.onClick.AddListener(() => OnResponseSelected(response));
-            }
+        foreach (ChatResponse response in message.responses)
+        {
+            GameObject responseGO = Instantiate(responseButtonPrefab, responseContent);
+            TMP_Text responseText = responseGO.GetComponentInChildren<TMP_Text>();
+            responseText.text = response.responseText;
+
+            Button responseButton = responseGO.GetComponentInChildren<Button>();
+            responseButton.onClick.AddListener(() => OnResponseSelected(response));
+            activeResponseButtons.Add(responseGO); // Track the active response button
         }
 
         currentMessageIndex++;
@@ -50,13 +51,16 @@ public class ChatManager : MonoBehaviour
     void OnResponseSelected(ChatResponse response)
     {
         // Clear old responses
-        foreach (Transform child in chatContent)
+        foreach (GameObject button in activeResponseButtons)
         {
-            if (child.GetComponent<Button>() != null)
-            {
-                Destroy(child.gameObject);
-            }
+            Destroy(button);
         }
+        activeResponseButtons.Clear();
+
+        // Add player message
+        GameObject playerMessageGO = Instantiate(playerMessagePrefab, chatContent);
+        TMP_Text playerMessageText = playerMessageGO.GetComponentInChildren<TMP_Text>();
+        playerMessageText.text = response.responseText;
 
         DisplayMessage(response.nextMessageIndex);
     }
