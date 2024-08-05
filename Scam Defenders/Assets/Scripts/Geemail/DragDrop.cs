8d821040444f7c9995bec2b87e61a2b9a4ee1161
sugondesenoots <martinmangahas972005@gmail.com>
2024-08-05 15:19:25 +0800

    mini-game fixes
    -fixed a bug where flag could be picked up during the tutorial

--- Scam Defenders/Assets/Scripts/Geemail/DragDrop.cs
+++ Scam Defenders/Assets/Scripts/Geemail/DragDrop.cs
@@ -1,45 +1,48 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class DragDrop : MonoBehaviour
+public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    Vector3 offset;
-    Collider2D collider2d;
+    private Vector2 offset;
+    private RectTransform rectTransform;
+    private Canvas canvas;
 
     public string destinationTag = "DropArea";
-    public string hotbarTag = "Hotbar"; 
+    public string hotbarTag = "Hotbar";
     public Color defaultColor = Color.white;
     public Color snapColor = Color.red;
     public Color hoverColor = Color.yellow;
 
-    private Camera mainCamera;
     private Transform snapTransform;
     private DropArea snapDropArea;
-    private DropArea previousDropArea; 
+    private DropArea previousDropArea;
 
     private bool isSnapped = false;
     private bool isInPlace = false;
 
-    public SpriteRenderer flagRenderer;
-    private Vector3 originalPosition; 
+    public Image flagRenderer;
+    private Vector2 originalPosition;
 
     void Awake()
     {
-        collider2d = GetComponent<Collider2D>();
-        mainCamera = Camera.main;
-        flagRenderer = GetComponent<SpriteRenderer>(); 
-        originalPosition = transform.position; //Store the starting position of the flag
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        flagRenderer = GetComponent<Image>();
+        originalPosition = rectTransform.anchoredPosition; // Store the starting position of the flag
     }
 
-    void OnMouseDown()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        offset = transform.position - MouseWorldPosition();
+        offset = rectTransform.anchoredPosition - GetAnchoredPosition(eventData);
         snapTransform = null;
         snapDropArea = null;
         isSnapped = false;
         isInPlace = false;
         flagRenderer.color = defaultColor;
 
-        //Store the previous drop area
+        // Store the previous drop area
         if (previousDropArea != null)
         {
             previousDropArea.isOccupied = false;
@@ -47,65 +50,75 @@ public class DragDrop : MonoBehaviour
         }
     }
 
-    void OnMouseDrag()
+    public void OnDrag(PointerEventData eventData)
     {
-        Vector3 newPos = MouseWorldPosition() + offset;
-        newPos = ClampToViewport(newPos);
-        transform.position = newPos;
-
-        //Reset color while dragging
+        rectTransform.anchoredPosition = GetAnchoredPosition(eventData) + offset;
         flagRenderer.color = defaultColor;
 
-        //Perform raycast to check for hover over drop area
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
-        if (hit.collider != null && hit.collider.CompareTag(destinationTag))
+        // Perform raycast to check for hover over drop area
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        pointerEventData.position = eventData.position;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, results);
+
+        foreach (RaycastResult result in results)
         {
-            DropArea hoverDropArea = hit.collider.GetComponent<DropArea>();
-            if (!hit.collider.CompareTag(hotbarTag) && !hoverDropArea.isOccupied)
+            if (result.gameObject.CompareTag(destinationTag))
             {
-                flagRenderer.color = hoverColor; //Change color on hover
+                DropArea hoverDropArea = result.gameObject.GetComponent<DropArea>();
+                if (!result.gameObject.CompareTag(hotbarTag) && !hoverDropArea.isOccupied)
+                {
+                    flagRenderer.color = hoverColor; // Change color on hover
+                }
+                break;
             }
         }
     }
 
-    void OnMouseUp()
+    public void OnEndDrag(PointerEventData eventData)
     {
-        collider2d.enabled = false;
-
-        //Release from previous drop area if snapped
+        // Release from previous drop area if snapped
         if (isSnapped && snapTransform != null)
         {
             snapDropArea.isOccupied = false;
             snapDropArea = null;
         }
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        pointerEventData.position = eventData.position;
 
-        if (hit.collider != null && hit.collider.CompareTag(destinationTag) && !hit.collider.CompareTag(hotbarTag))
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, results);
+
+        foreach (RaycastResult result in results)
         {
-            DropArea newDropArea = hit.collider.GetComponent<DropArea>();
-            if (!newDropArea.isOccupied)
+            if (result.gameObject.CompareTag(destinationTag) && !result.gameObject.CompareTag(hotbarTag))
             {
-                snapTransform = hit.transform;
-                snapDropArea = hit.collider.GetComponent<DropArea>();
-                isSnapped = true;
+                DropArea newDropArea = result.gameObject.GetComponent<DropArea>();
+                if (!newDropArea.isOccupied)
+                {
+                    snapTransform = result.gameObject.transform;
+                    snapDropArea = result.gameObject.GetComponent<DropArea>();
+                    isSnapped = true;
 
-                transform.position = snapTransform.position + new Vector3(0, 0, -0.01f);
-                isInPlace = true;
-                snapDropArea.isOccupied = true; //Mark the drop area as occupied
-                previousDropArea = snapDropArea; //Update the previous drop area
+                    rectTransform.anchoredPosition = ((RectTransform)snapTransform).anchoredPosition;
+                    isInPlace = true;
+                    snapDropArea.isOccupied = true; // Mark the drop area as occupied
+                    previousDropArea = snapDropArea; // Update the previous drop area
+                    break;
+                }
+                else
+                {
+                    ResetToOriginalPosition();
+                }
             }
             else
             {
                 ResetToOriginalPosition();
             }
         }
-        else
-        {
-            ResetToOriginalPosition();
-        }
 
-        collider2d.enabled = true;
         UpdateColor();
     }
 
@@ -116,8 +129,8 @@ public class DragDrop : MonoBehaviour
         isSnapped = false;
         isInPlace = false;
 
-        //Reset to original position if not snapped to a new drop area
-        transform.position = originalPosition;
+        // Reset to original position if not snapped to a new drop area
+        rectTransform.anchoredPosition = originalPosition;
     }
 
     void UpdateColor()
@@ -132,18 +145,10 @@ public class DragDrop : MonoBehaviour
         }
     }
 
-    Vector3 MouseWorldPosition()
+    Vector2 GetAnchoredPosition(PointerEventData eventData)
     {
-        var mouseScreenPos = Input.mousePosition;
-        mouseScreenPos.z = mainCamera.WorldToScreenPoint(transform.position).z;
-        return mainCamera.ScreenToWorldPoint(mouseScreenPos);
-    }
-
-    Vector3 ClampToViewport(Vector3 position)
-    {
-        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(position);
-        viewportPosition.x = Mathf.Clamp(viewportPosition.x, 0.0f, 1.0f);
-        viewportPosition.y = Mathf.Clamp(viewportPosition.y, 0.0f, 1.0f);
-        return mainCamera.ViewportToWorldPoint(viewportPosition);
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, canvas.worldCamera, out localPoint);
+        return localPoint;
     }
 }
