ca95567d3b4614ef63cf6d5dd4eba2d4f390d71a
Hwang <chrishuangyc@gmail.com>
2024-08-05 11:48:46 +0800

    Updated transitions and UI

--- Scam Defenders/Assets/Scripts/ChatScenario.cs
+++ Scam Defenders/Assets/Scripts/ChatScenario.cs
@@ -12,7 +12,6 @@ public class ChatScenario : ScriptableObject
 public class ChatMessage
 {
     public string messageText;
-    //public bool isPlayerMessage; // To distinguish between player and NPC messages
     public List<ChatResponse> responses; // Only used for NPC messages
 }
 
@@ -20,6 +19,5 @@ public class ChatMessage
 public class ChatResponse
 {
     public string responseText;
-    //public bool isCorrect;
     public int nextMessageIndex; // Index of the next message in the scenario
 }
