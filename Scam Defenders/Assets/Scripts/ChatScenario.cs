a405f3f87da5238f19ce101103b390fa9ed6447e
Hwang <chrishuangyc@gmail.com>
2024-08-16 16:20:31 +0800

    Updated love scam

--- Scam Defenders/Assets/Scripts/ChatScenario.cs
+++ Scam Defenders/Assets/Scripts/ChatScenario.cs
@@ -11,15 +11,23 @@ public class ChatScenario : ScriptableObject
 [System.Serializable]
 public class ChatMessage
 {
-    public string messageText;
+    //public string messageText;
+    public List<string> messageTexts; // List of messages to be sent in a row
     public List<ChatResponse> responses; // Only used for NPC messages
 }
 
 [System.Serializable]
 public class ChatResponse
 {
-    public string responseText;
-    public bool isCorrect;
-    public bool isWrong;
+    //public string responseText;
+    public List<string> responseTexts; // List of player response messages
+    public bool winScreen1;
+    public bool winScreen2;
+    public bool winScreen3;
+    public bool winScreen4;
+    public bool loseScreen1;
+    public bool loseScreen2;
+    public bool loseScreen3;
+    public bool loseScreen4;
     public int nextMessageIndex; // Index of the next message in the scenario
 }
