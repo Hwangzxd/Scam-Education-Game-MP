b36807fa7427f815cf21ecf361376259df134e16
sugondesenoots <martinmangahas972005@gmail.com>
2024-09-04 18:45:17 +0800

    randomizer for yabber scenarios

--- Scam Defenders/Assets/Scripts/Managers/SceneRandomizer.cs
+++ Scam Defenders/Assets/Scripts/Managers/SceneRandomizer.cs
@@ -5,6 +5,8 @@ public class SceneRandomizer : MonoBehaviour
 {
     public string[] shopEaseScenes = { "ShopEase", "ShopEase2", "ShopEase3" };
     public string[] birdyNetScenes = { "LoveScamChatNew1", "LoveScamChatNew2", "LoveScamChatNew3" };
+    public string[] yabberChatINVSScenes = { "YabberChatINVS", "YabberChatINVS1", "YabberChatINVS2" };
+    public string[] yabberChatGOISScenes = { "YabberChatGOIS", "YabberChatGOIS1", "YabberChatGOIS2" };
 
     public void LoadRandomSceneShopEase()
     {
@@ -16,5 +18,17 @@ public class SceneRandomizer : MonoBehaviour
     {
         int randomIndex = Random.Range(0, birdyNetScenes.Length);
         SceneManager.LoadScene(birdyNetScenes[randomIndex]);
+    }
+
+    public void LoadRandomSceneINVS()
+    {
+        int randomIndex = Random.Range(0, yabberChatINVSScenes.Length);
+        SceneManager.LoadScene(yabberChatINVSScenes[randomIndex]);
+    }
+
+    public void LoadRandomSceneGOIS()
+    {
+        int randomIndex = Random.Range(0, yabberChatGOISScenes.Length);
+        SceneManager.LoadScene(yabberChatGOISScenes[randomIndex]);
     }
 }
